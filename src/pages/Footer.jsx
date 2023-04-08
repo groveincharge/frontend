9f6f915d6ebb03d99255872bd41a28501997a7d7
groveincharge <wdasent@yahoo.com>
2023-04-08 02:06:57 -0400

    replacing my App and Footer files that i accidentally erased.

---
+++ src/pages/Footer.jsx
@@ -0,0 +1,37 @@
+import intro from "./../projectvideos/intro.mp4";
+
+const Footer = () => {
+
+return(
+<div>
+<footer className="page-footer font-small mdb-color darken-3 pt-4">
+
+    <div className="row d-flex justify-content-center">
+
+      <div className="col-md-6">
+        <div className="embed-responsive embed-responsive-16by9 mb-4">
+          <iframe className="embed-responsive-item" src={intro}
+            allowFullScreen title="topic video"></iframe>
+        </div>
+      </div>
+
+      <div className="col-md-6">
+      <h5 className="text-uppercase font-weight-bold">Footer text 1</h5>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Expedita sapiente sint, nulla, nihil
+          repudiandae commodi voluptatibus corrupti animi sequi aliquid magnam debitis, maxime quam recusandae
+          harum esse fugiat. Itaque, culpa?
+        </p>
+    </div>
+
+  </div>
+
+  <div className="footer-copyright text-center py-3">© 2023 Copyright:
+    <a href="/"> MDBootstrap.com</a>
+  </div>
+
+</footer>
+</div>
+)
+}
+
+export default Footer;
